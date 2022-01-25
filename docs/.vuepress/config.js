cb00226f4050cf26bd08974c766c581f3adb17dd
shuo.tang <shuo.tang@colourdata.com.cn>
2022-01-25 13:36:40 +0800

    feat: change config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,9 @@ module.exports = {
       lang: 'zh-CN'
     }
   },
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
   themeConfig: {
     editLinks: false,
     docsDir: 'docs',
